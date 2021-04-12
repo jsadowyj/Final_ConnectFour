f10e357d15afbc432ba2150b6d2cf39c76238d22
kbartram03 <kbartram03@gmail.com>
2021-04-12 19:45:53 -0400

    Add second form

--- Final_ConnectFour/WelcomeForm.Designer.cs
+++ Final_ConnectFour/WelcomeForm.Designer.cs
@@ -43,9 +43,10 @@ namespace Final_ConnectFour
             // 
             this.btn_1player.BackColor = System.Drawing.Color.Turquoise;
             this.btn_1player.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_1player.Location = new System.Drawing.Point(113, 262);
+            this.btn_1player.Location = new System.Drawing.Point(151, 322);
+            this.btn_1player.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_1player.Name = "btn_1player";
-            this.btn_1player.Size = new System.Drawing.Size(237, 148);
+            this.btn_1player.Size = new System.Drawing.Size(316, 182);
             this.btn_1player.TabIndex = 0;
             this.btn_1player.Text = "1 Player";
             this.btn_1player.UseVisualStyleBackColor = false;
@@ -54,9 +55,10 @@ namespace Final_ConnectFour
             // 
             this.btn_2player.BackColor = System.Drawing.Color.Turquoise;
             this.btn_2player.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_2player.Location = new System.Drawing.Point(462, 262);
+            this.btn_2player.Location = new System.Drawing.Point(616, 322);
+            this.btn_2player.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_2player.Name = "btn_2player";
-            this.btn_2player.Size = new System.Drawing.Size(237, 148);
+            this.btn_2player.Size = new System.Drawing.Size(316, 182);
             this.btn_2player.TabIndex = 1;
             this.btn_2player.Text = "2 Player";
             this.btn_2player.UseVisualStyleBackColor = false;
@@ -65,9 +67,10 @@ namespace Final_ConnectFour
             // 
             this.lbl_1.AutoSize = true;
             this.lbl_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_1.Location = new System.Drawing.Point(264, 9);
+            this.lbl_1.Location = new System.Drawing.Point(352, 11);
+            this.lbl_1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl_1.Name = "lbl_1";
-            this.lbl_1.Size = new System.Drawing.Size(275, 46);
+            this.lbl_1.Size = new System.Drawing.Size(341, 58);
             this.lbl_1.TabIndex = 2;
             this.lbl_1.Text = "Connect Four";
             this.lbl_1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -76,9 +79,10 @@ namespace Final_ConnectFour
             // 
             this.btn_exit.BackColor = System.Drawing.Color.Red;
             this.btn_exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_exit.Location = new System.Drawing.Point(356, 408);
+            this.btn_exit.Location = new System.Drawing.Point(475, 502);
+            this.btn_exit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_exit.Name = "btn_exit";
-            this.btn_exit.Size = new System.Drawing.Size(100, 51);
+            this.btn_exit.Size = new System.Drawing.Size(133, 63);
             this.btn_exit.TabIndex = 3;
             this.btn_exit.Text = "EXIT";
             this.btn_exit.UseVisualStyleBackColor = false;
@@ -87,9 +91,10 @@ namespace Final_ConnectFour
             // 
             this.lbl_wins.AutoSize = true;
             this.lbl_wins.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_wins.Location = new System.Drawing.Point(199, 85);
+            this.lbl_wins.Location = new System.Drawing.Point(265, 105);
+            this.lbl_wins.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl_wins.Name = "lbl_wins";
-            this.lbl_wins.Size = new System.Drawing.Size(89, 31);
+            this.lbl_wins.Size = new System.Drawing.Size(111, 39);
             this.lbl_wins.TabIndex = 4;
             this.lbl_wins.Text = "WINS";
             // 
@@ -97,9 +102,10 @@ namespace Final_ConnectFour
             // 
             this.lbl_losses.AutoSize = true;
             this.lbl_losses.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_losses.Location = new System.Drawing.Point(517, 85);
+            this.lbl_losses.Location = new System.Drawing.Point(689, 105);
+            this.lbl_losses.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl_losses.Name = "lbl_losses";
-            this.lbl_losses.Size = new System.Drawing.Size(128, 31);
+            this.lbl_losses.Size = new System.Drawing.Size(160, 39);
             this.lbl_losses.TabIndex = 5;
             this.lbl_losses.Text = "LOSSES";
             // 
@@ -108,9 +114,10 @@ namespace Final_ConnectFour
             this.wins_count.BackColor = System.Drawing.Color.DodgerBlue;
             this.wins_count.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.wins_count.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.wins_count.Location = new System.Drawing.Point(188, 145);
+            this.wins_count.Location = new System.Drawing.Point(251, 178);
+            this.wins_count.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.wins_count.Name = "wins_count";
-            this.wins_count.Size = new System.Drawing.Size(100, 31);
+            this.wins_count.Size = new System.Drawing.Size(133, 38);
             this.wins_count.TabIndex = 6;
             this.wins_count.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
@@ -119,18 +126,19 @@ namespace Final_ConnectFour
             this.losses_count.BackColor = System.Drawing.Color.DodgerBlue;
             this.losses_count.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.losses_count.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.losses_count.Location = new System.Drawing.Point(523, 145);
+            this.losses_count.Location = new System.Drawing.Point(697, 178);
+            this.losses_count.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.losses_count.Name = "losses_count";
-            this.losses_count.Size = new System.Drawing.Size(100, 31);
+            this.losses_count.Size = new System.Drawing.Size(133, 38);
             this.losses_count.TabIndex = 7;
             this.losses_count.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // WelcomeForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DodgerBlue;
-            this.ClientSize = new System.Drawing.Size(800, 459);
+            this.ClientSize = new System.Drawing.Size(1067, 565);
             this.Controls.Add(this.losses_count);
             this.Controls.Add(this.wins_count);
             this.Controls.Add(this.lbl_losses);
@@ -139,8 +147,8 @@ namespace Final_ConnectFour
             this.Controls.Add(this.lbl_1);
             this.Controls.Add(this.btn_2player);
             this.Controls.Add(this.btn_1player);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "WelcomeForm";
-            this.Text = "Form2";
             this.Load += new System.EventHandler(this.WelcomeForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
