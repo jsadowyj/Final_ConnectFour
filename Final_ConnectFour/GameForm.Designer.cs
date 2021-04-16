480b2e26bd57a9922a0481c1a696e8972bccb15c
jsadowyj0 <sadowyjj@gmail.com>
2021-04-15 20:40:08 -0400

    Add Round Buttons (and pray)

--- Final_ConnectFour/GameForm.Designer.cs
+++ Final_ConnectFour/GameForm.Designer.cs
@@ -31,9 +31,14 @@ namespace Final_ConnectFour
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameForm));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.roundBtn_0_0 = new Final_ConnectFour.RoundButton();
+            this.roundButton1 = new Final_ConnectFour.RoundButton();
+            this.roundButton2 = new Final_ConnectFour.RoundButton();
+            this.roundButton3 = new Final_ConnectFour.RoundButton();
+            this.roundButton4 = new Final_ConnectFour.RoundButton();
+            this.roundButton5 = new Final_ConnectFour.RoundButton();
+            this.roundButton6 = new Final_ConnectFour.RoundButton();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -41,42 +46,111 @@ namespace Final_ConnectFour
             this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(1, 0);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(800, 730);
+            this.pictureBox1.Size = new System.Drawing.Size(600, 593);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // pictureBox2
+            // roundBtn_0_0
             // 
-            this.pictureBox2.BackColor = System.Drawing.SystemColors.Control;
-            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(127, 21);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(95, 94);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
+            this.roundBtn_0_0.BackColor = System.Drawing.SystemColors.Control;
+            this.roundBtn_0_0.FlatAppearance.BorderSize = 0;
+            this.roundBtn_0_0.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.roundBtn_0_0.Location = new System.Drawing.Point(9, 16);
+            this.roundBtn_0_0.Name = "roundBtn_0_0";
+            this.roundBtn_0_0.Size = new System.Drawing.Size(75, 79);
+            this.roundBtn_0_0.TabIndex = 1;
+            this.roundBtn_0_0.UseVisualStyleBackColor = false;
+            // 
+            // roundButton1
+            // 
+            this.roundButton1.BackColor = System.Drawing.SystemColors.Control;
+            this.roundButton1.FlatAppearance.BorderSize = 0;
+            this.roundButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.roundButton1.Location = new System.Drawing.Point(93, 16);
+            this.roundButton1.Name = "roundButton1";
+            this.roundButton1.Size = new System.Drawing.Size(75, 79);
+            this.roundButton1.TabIndex = 1;
+            this.roundButton1.UseVisualStyleBackColor = false;
+            // 
+            // roundButton2
+            // 
+            this.roundButton2.BackColor = System.Drawing.SystemColors.Control;
+            this.roundButton2.FlatAppearance.BorderSize = 0;
+            this.roundButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.roundButton2.Location = new System.Drawing.Point(178, 15);
+            this.roundButton2.Name = "roundButton2";
+            this.roundButton2.Size = new System.Drawing.Size(75, 79);
+            this.roundButton2.TabIndex = 1;
+            this.roundButton2.UseVisualStyleBackColor = false;
+            // 
+            // roundButton3
+            // 
+            this.roundButton3.BackColor = System.Drawing.SystemColors.Control;
+            this.roundButton3.FlatAppearance.BorderSize = 0;
+            this.roundButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.roundButton3.Location = new System.Drawing.Point(263, 15);
+            this.roundButton3.Name = "roundButton3";
+            this.roundButton3.Size = new System.Drawing.Size(75, 79);
+            this.roundButton3.TabIndex = 1;
+            this.roundButton3.UseVisualStyleBackColor = false;
+            // 
+            // roundButton4
+            // 
+            this.roundButton4.BackColor = System.Drawing.SystemColors.Control;
+            this.roundButton4.FlatAppearance.BorderSize = 0;
+            this.roundButton4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.roundButton4.Location = new System.Drawing.Point(348, 16);
+            this.roundButton4.Name = "roundButton4";
+            this.roundButton4.Size = new System.Drawing.Size(75, 79);
+            this.roundButton4.TabIndex = 1;
+            this.roundButton4.UseVisualStyleBackColor = false;
+            // 
+            // roundButton5
+            // 
+            this.roundButton5.BackColor = System.Drawing.SystemColors.Control;
+            this.roundButton5.FlatAppearance.BorderSize = 0;
+            this.roundButton5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.roundButton5.Location = new System.Drawing.Point(432, 15);
+            this.roundButton5.Name = "roundButton5";
+            this.roundButton5.Size = new System.Drawing.Size(75, 79);
+            this.roundButton5.TabIndex = 1;
+            this.roundButton5.UseVisualStyleBackColor = false;
+            // 
+            // roundButton6
+            // 
+            this.roundButton6.BackColor = System.Drawing.SystemColors.Control;
+            this.roundButton6.FlatAppearance.BorderSize = 0;
+            this.roundButton6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.roundButton6.Location = new System.Drawing.Point(517, 15);
+            this.roundButton6.Name = "roundButton6";
+            this.roundButton6.Size = new System.Drawing.Size(75, 79);
+            this.roundButton6.TabIndex = 1;
+            this.roundButton6.UseVisualStyleBackColor = false;
             // 
             // GameForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(804, 834);
-            this.Controls.Add(this.pictureBox2);
+            this.ClientSize = new System.Drawing.Size(603, 678);
+            this.Controls.Add(this.roundButton6);
+            this.Controls.Add(this.roundButton5);
+            this.Controls.Add(this.roundButton4);
+            this.Controls.Add(this.roundButton3);
+            this.Controls.Add(this.roundButton2);
+            this.Controls.Add(this.roundButton1);
+            this.Controls.Add(this.roundBtn_0_0);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.Name = "GameForm";
             this.Text = "Connect Four";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.GameForm_FormClosing);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -84,6 +158,12 @@ namespace Final_ConnectFour
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
+        private RoundButton roundBtn_0_0;
+        private RoundButton roundButton1;
+        private RoundButton roundButton2;
+        private RoundButton roundButton3;
+        private RoundButton roundButton4;
+        private RoundButton roundButton5;
+        private RoundButton roundButton6;
     }
 }
